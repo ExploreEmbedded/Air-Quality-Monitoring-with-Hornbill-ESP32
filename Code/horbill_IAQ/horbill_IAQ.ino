171e69a29338e1ad439a94acc7c40fee4ec24a70
raghavendrahassy <raghavendra@exploreembedded.com>
2017-02-01 16:28:22 +0530

    Data logged on ThingSpeak

--- Code/horbill_IAQ/horbill_IAQ.ino
+++ Code/horbill_IAQ/horbill_IAQ.ino
@@ -1,51 +1,16 @@
-/*
 
-Copyright (c) 2015, Embedded Adventures
-All rights reserved.
 
-Contact us at source [at] embeddedadventures.com
-www.embeddedadventures.com
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-- Redistributions of source code must retain the above copyright notice,
-  this list of conditions and the following disclaimer.
-
-- Redistributions in binary form must reproduce the above copyright
-  notice, this list of conditions and the following disclaimer in the
-  documentation and/or other materials provided with the distribution.
-
-- Neither the name of Embedded Adventures nor the names of its contributors
-  may be used to endorse or promote products derived from this software
-  without specific prior written permission.
- 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
-THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
-
-// Test sketch for MOD1023 
 // Communicating with  indoor air quality sensor
 
-
+#include <WiFi.h>
 #include <iAQ-MOD1023.h>
 #include "Adafruit_SHT31.h"
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 #define OLED_RESET 4
+#define OLED_address  0x3c
 Adafruit_SSD1306 display(OLED_RESET);
 
 #define NUMFLAKES 10
@@ -53,23 +18,59 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define YPOS 1
 #define DELTAY 2
 
+String apiKey = "YK4CWRHAS88E3E3E";
+char ssid[20] = "ExploreEmbedded.com";
+char password[20] = "9632083055";
+char TempValue[15];
+String str;
+const char* server = "api.thingspeak.com";
+
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
 float t,h;
 unsigned int  pred,tvoc;
+WiFiClient client;
 
 void setup() {
   Serial.begin(115200);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
   display.clearDisplay();
+
+  display.setCursor(0,20);
+  display.print("Connecting to:");
+
+  display.setCursor(0,40);
+  display.print(ssid);
+  
+    // Setup the Internet
+  WiFi.begin(ssid, password);
+  int pos  = 0;
+  int dot = 20;
+  while (WiFi.status() != WL_CONNECTED) {
+
+    delay(500);
+    pos++;
+    dot++;
+    display.setCursor(dot,40);
+    display.print(".");
+   
+  }
+
+ // sendStrXY("connected", 5, 0);
+  display.clearDisplay();
+  delay(100); 
+  
+  
   
   }
 
 void loop() {
+  wifi_thingspeak();
   iaqUpdate();  
   readSensors();
   OLED_Display();
+  
   
   
 }
@@ -88,9 +89,9 @@ void iaqUpdate() {
  // Serial.println("Indoor Air Quality Data");
   //Serial.print("iAQ Data Status: \t");
   //Serial.println(iaq.getStatus());
-  Serial.println(pred);
+ // Serial.println(pred);
  // Serial.println(ohm);
-  Serial.println(tvoc);
+ // Serial.println(tvoc);
   //Serial.println();
 }
 
@@ -105,20 +106,30 @@ void readSensors(){
   
            t = sht31.readTemperature();
            h = sht31.readHumidity();
+           char tvalue[5], hvalue[5];
         
           if (! isnan(t)) {  // check if 'is not a number'
-            Serial.print("Temp *C = "); Serial.println(t);
+           // Serial.print("Temp *C = "); Serial.println(t);
           } else { 
             Serial.println("Failed to read temperature");
           }
           
           if (! isnan(h)) {  // check if 'is not a number'
-            Serial.print("Hum. % = "); Serial.println(h);
+           // Serial.print("Hum. % = "); Serial.println(h);
           } else { 
             Serial.println("Failed to read humidity");
           }
           Serial.println();
           //delay(1000); 
+
+         //str = String(t);
+         //str.toCharArray(tvalue,15); 
+         //sendStrXY(tvalue,3,10);
+
+         //str = String(h);
+        // str.toCharArray(hvalue,15); 
+         //sendStrXY(hvalue,4,10);
+
  }
 
 void OLED_Display()
@@ -171,8 +182,51 @@ void OLED_Display()
 
   
   display.display();
- 
+}
+
+void wifi_thingspeak()
+{
+
+         
+  if (client.connect(server,80)) {  //   "184.106.153.149" or api.thingspeak.com
+              String postStr = apiKey;
+                     postStr +="&field1=";
+                     postStr += String(t);
+                     postStr +="&field2=";
+                     postStr += String(h);
+                     postStr +="&field3=";
+                     postStr += String(pred);
+                     postStr +="&field4=";
+                     postStr += String(tvoc);
+                     postStr += "\r\n\r\n";
+           
+               client.print("POST /update HTTP/1.1\n"); 
+               client.print("Host: api.thingspeak.com\n"); 
+               client.print("Connection: close\n"); 
+               client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n"); 
+               client.print("Content-Type: application/x-www-form-urlencoded\n"); 
+               client.print("Content-Length: "); 
+               client.print(postStr.length()); 
+               client.print("\n\n"); 
+               client.print(postStr);
+                     
+           
+               Serial.print("Temperature: ");
+               Serial.print(t);
+               Serial.print(" degrees Celcius Humidity: "); 
+               Serial.print(h);
+               Serial.println("% send to Thingspeak");    
+            }
+            client.stop();
+             
+            Serial.println("Waiting...");    
+            // thingspeak needs minimum 15 sec delay between updates
+            delay(20000);  
+     
+
 
 
 }
+
+
 
